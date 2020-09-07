2aeccdc2f6d787de5392c9e754065d359d11692c
bulat <bulat.i.safin@gmail.com>
2020-09-07 10:21:29 +0300

    Updates

---
+++ precache-manifest.de67b1856a007c9e2ea15e7085d5f178.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def780550af83d87c6aafb75a33e4c88",
+    "url": "/fb2-reader/index.html"
+  },
+  {
+    "revision": "e70a299da5a822a64ff3",
+    "url": "/fb2-reader/static/css/main.2a881d77.chunk.css"
+  },
+  {
+    "revision": "b4eb6c4e6537565c8d4c",
+    "url": "/fb2-reader/static/js/2.7a537c82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fb2-reader/static/js/2.7a537c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70a299da5a822a64ff3",
+    "url": "/fb2-reader/static/js/main.5923aa5c.chunk.js"
+  },
+  {
+    "revision": "f9d639825e5abdfb2e3b",
+    "url": "/fb2-reader/static/js/runtime-main.3e7da4f1.js"
+  },
+  {
+    "revision": "98abdde47e9de09a5468834f0e9491b8",
+    "url": "/fb2-reader/static/media/arrow_back.98abdde4.svg"
+  },
+  {
+    "revision": "204f61a19eb3371c4df719a3ef5b957c",
+    "url": "/fb2-reader/static/media/menu.204f61a1.svg"
+  }
+]);
