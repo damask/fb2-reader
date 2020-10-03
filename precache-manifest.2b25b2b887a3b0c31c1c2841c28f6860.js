742cf955def9a9f595a62846496e3a45b0173747
bsafin <71329866+bsafin@users.noreply.github.com>
2020-10-03 04:26:54 +0000

    Deploying to gh-pages from  @ 0d4a339f2cfeb6b1aa599ec6f37ebfaabba80053 🚀

---
+++ precache-manifest.2b25b2b887a3b0c31c1c2841c28f6860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4967d967d1865701cc59149d705898",
+    "url": "/fb2-reader/index.html"
+  },
+  {
+    "revision": "cab08775349e26e946e8",
+    "url": "/fb2-reader/static/css/main.68df3915.chunk.css"
+  },
+  {
+    "revision": "55793f91d6af30720f12",
+    "url": "/fb2-reader/static/js/2.d6317a4a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fb2-reader/static/js/2.d6317a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab08775349e26e946e8",
+    "url": "/fb2-reader/static/js/main.b64b60e9.chunk.js"
+  },
+  {
+    "revision": "f9d639825e5abdfb2e3b",
+    "url": "/fb2-reader/static/js/runtime-main.3e7da4f1.js"
+  }
+]);
